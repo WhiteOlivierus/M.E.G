4e71f9f2e62f7d92f26d60e71ed57dc5abacebc3
Bastien <bas.dijkstra@student.hku.nl>
2019-03-27 13:56:44 +0100

    Whut

---
+++ Assets/Scripts/Utilities/Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject homeScreen;
+    public GameObject pauseScreen;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseScreen.SetActive(!pauseScreen.activeSelf);
+        }
+    }
+
+    public void LoadScene(int index)
+    {
+        homeScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+        SceneManager.LoadScene(index);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
